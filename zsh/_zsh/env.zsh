e8949ac05039ee3e7a8f918027c44f8fc71284af
Aimee Langmaid <aimee@aimeeble.com>
2012-10-15 09:21:52 -0700

    zsh: set editor; add local python tools to path

--- zsh/_zsh/env.zsh
+++ zsh/_zsh/env.zsh
@@ -1,6 +1,10 @@
 echo "env"
 
+[[ -d /usr/local/share/python ]] && PATH="/usr/local/share/python:$PATH"
 export PATH="$HOME/bin:/usr/local/bin:$PATH"
+
+export VISUAL=vim
+export EDITOR=vim
 
 # Pull in various other config files
 PLATFORMRC="$HOME/.zshenv-`uname -s`"
