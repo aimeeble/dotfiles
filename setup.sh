f5f2cd797df1d4471d4a786cdc92993ce7b2842c
Aimee <aimee@aimeeble.com>
2014-01-31 21:28:11 -0800

    weechat: add screen_away plugin

--- setup.sh
+++ setup.sh
@@ -90,11 +90,13 @@ link_weechat() {
     linkit "$DOTFILE_PATH/weechat/python/notify.py" "$INSTALL_PATH/.weechat/python/notify.py"
     linkit "$DOTFILE_PATH/weechat/python/title.py" "$INSTALL_PATH/.weechat/python/title.py"
     linkit "$DOTFILE_PATH/weechat/python/tmux_env.py" "$INSTALL_PATH/.weechat/python/tmux_env.py"
+    linkit "$DOTFILE_PATH/weechat/python/screen_away.py" "$INSTALL_PATH/.weechat/python/screen_away.py"
     linkit "$DOTFILE_PATH/weechat/perl/iset.pl" "$INSTALL_PATH/.weechat/perl/iset.pl"
 
     # Auto-load plugins
     linkit "$DOTFILE_PATH/weechat/python/title.py" "$INSTALL_PATH/.weechat/python/autoload/title.py"
     linkit "$DOTFILE_PATH/weechat/python/tmux_env.py" "$INSTALL_PATH/.weechat/python/autoload/tmux_env.py"
+    linkit "$DOTFILE_PATH/weechat/python/screen_away.py" "$INSTALL_PATH/.weechat/python/autoload/screen_away.py"
     linkit "$DOTFILE_PATH/weechat/perl/iset.pl" "$INSTALL_PATH/.weechat/perl/autoload/iset.pl"
 }
 
