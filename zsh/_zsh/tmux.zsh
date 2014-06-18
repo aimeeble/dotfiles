31e911385f214a3e4e7b6b8b9fc9f726ca33b67e
Aimee <aimee@aimeeble.com>
2014-06-18 11:33:03 -0700

    zsh: tmux conf actually exports vars; add force option too

--- zsh/_zsh/tmux.zsh
+++ zsh/_zsh/tmux.zsh
@@ -5,7 +5,22 @@ precmd_tmux_env_update() {
     return
   fi
 
-  typeset -g TMUX_ENV_VARS
+
+  local FORCE
+  if [[ -n "$1" ]]; then
+    FORCE="yes"
+  fi
+
+  local CURRENT_TMUX_CLIENT
+  CURRENT_TMUX_CLIENT=$(tmux display-message -p '#{client_tty}')
+
+  if [[ -n "$LAST_TMUX_CLIENT" && "$CURRENT_TMUX_CLIENT" != "$LAST_TMUX_CLIENT" ]]; then
+    print -P "%F{yellow}Client changed--updating all vars%f"
+    FORCE="yes"
+  fi
+  LAST_TMUX_CLIENT=$CURRENT_TMUX_CLIENT
+
+  typeset -l TMUX_ENV_VARS
   TMUX_ENV_VARS=("${(@f)$(tmux show-environment)}")
   for var in $TMUX_ENV_VARS; do
     KEY="$(echo $var | cut -d'=' -f1)"
@@ -13,16 +28,16 @@ precmd_tmux_env_update() {
     if [[ "${KEY:0:1}" == "-" ]]; then
       OLDVAL="${(P)${KEY:1}}"
       # unset
-      if [[ -n "${OLDVAL}" ]]; then
+      if [[ -n "${OLDVAL}" || -n "$FORCE" ]]; then
         print -P "%F{red}Unsetting%f ${KEY:1} (was: '${OLDVAL}')"
         unset "${KEY:1}"
       fi
     else
       OLDVAL="${(P)KEY}"
       # set
-      if [[ "${OLDVAL}" != "${VAL}" ]]; then
+      if [[ "${OLDVAL}" != "${VAL}" || -n "$FORCE" ]]; then
         print -P "%F{green}Updating%f ${KEY}='${VAL}' (was: '${OLDVAL}')"
-        eval "$KEY='$VAL'"
+        eval export "$KEY='$VAL'"
       fi
     fi
   done
