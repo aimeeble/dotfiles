d0dc1fba5173fcbacca5ec108f246eefc216eefb
Aimee <aimee@aimeeble.com>
2015-09-04 05:03:40 -0700

    zsh: virtenvwrap python3

--- zsh/_zsh/env.zsh
+++ zsh/_zsh/env.zsh
@@ -3,6 +3,7 @@ export PATH="$HOME/bin:/usr/local/bin:/usr/local/sbin:$PATH"
 export VISUAL=vim
 export EDITOR=vim
 export VIRTUAL_ENV_DISABLE_PROMPT=1
+export VIRTUALENVWRAPPER_PYTHON=/usr/local/bin/python3
 export WORKON_HOME=$HOME/.venvs
 
 # Pull in various other config files
