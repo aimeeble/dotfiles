8a2644c7e80cd38d284535c331c7baffa5ba0add
Aimee <aimee@aimeeble.com>
2020-02-05 11:29:11 -0800

    zsh: include tmux session in touchbar text

--- zsh/_zsh/modules.d/99-touchbar.zsh
+++ zsh/_zsh/modules.d/99-touchbar.zsh
@@ -9,12 +9,21 @@ _aimee_touchbar_ping() {
     return
   fi
 
-  local PR_MOD_FRAGMENTS
+  local TBAR_TEXT
+  if [[ -n "$TMUX" ]]; then
+    TBAR_TEXT="tmux:$(tmux display-message -p '#S')"
+  fi
+
+  local FRAGS
   for pf in $_aimee_mod_fragments; do
       local NEW="$($pf no)"
-      PR_MOD_FRAGMENTS="${PR_MOD_FRAGMENTS}${NEW}"
+      FRAGS="${FRAGS}${NEW}"
   done
-  "$HOME/.iterm2/it2setkeylabel" set status "${PR_MOD_FRAGMENTS}"
+  if [[ -n "$FRAGS" ]]; then
+    TBAR_TEXT="${TBAR_TEXT}    ${FRAGS}"
+  fi
+
+  "$HOME/.iterm2/it2setkeylabel" set status "${TBAR_TEXT}"
 }
 
 module_add "MBP Touchbar" _aimee_touchbar_init
